1b29f40b1b05d3bf58bb9b99d50bb56e8791e8a9
bruno benedetti <bbenedetti854@gmail.com>
2022-08-02 23:56:22 -0300

    uso de dom

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ function pay (){
 let titulo = document.getElementById("titulo");
 console.log(titulo);
 /* NODOS */
-/* para ver cant de prendas) */
+/* para ver cant de prendas */
 console.log(document.getElementsByTagName("h3"));
 console.log(document.querySelectorAll("h2"));
 
@@ -24,20 +24,8 @@ let listaPrendas = document.querySelectorAll("h2")
 
 for(let prendas of listaPrendas) {
     console.log(`esta prenda tiene un costo de  ${prendas.innerText}`)
-} 
+}
 
-console.log(titulo.innerText);
-titulo.innerText = "ideas para outfits";
-titulo.innerHTML = "<strong>agrego html</strong>";
-
-titulo.style.color = "red";
-
-titulo.className= "container row"; // cambio de clase //
-titulo.setAttribute("class", "titulo1"); //cambio de atributo(valor a modificar, valor deseado//
-
-/* let link = document.querySelector("a");
-link.setAttribute("href", "https://www.google.com/");
- */
 //crear HTML
 let subtitulo = document.createElement("h2");
 subtitulo.setAttribute("id", "nuevoElemento");
@@ -46,3 +34,50 @@ console.log(subtitulo);
 
 let a = document.querySelector('a')
 a.setAttribute('href', 'https://www.google.com.ar/')
+
+//Nuevos elementos, ingresos de ropa//
+/* let ingresos = document.getElementById("indumentaria");
+let indumentaria = ["chombaT", "chombaL", "basica","calza"];
+
+for(const ingreso of ingresos){
+    let li = document.createElement("li");
+    li.innerHTML = ingreso
+    ingresos.appendChild(li);
+    
+    console.log(ingresos);
+    console.log(indumentaria);
+    console.log(ingreso);
+
+}
+let producto = { id: 1, nombre: "chombaT", precio: 3500};
+let concatenando = "ID : " + producto.id + "- Producto" + producto.nombre + "$" + producto.precio; 
+  */
+class Producto{
+    constructor(prenda, categoria, costo){
+        this.prenda=prenda;
+        this.categoria=categoria;
+        this.costo = costo; 
+    }
+}
+let listaProductos = [
+    {prenda: "chomba", categoria:"indumentaria", costo:2000},
+    {prenda:"calza", categoria:"indumentaria", costo:1000},
+    {prenda:"buzo", categoria:"indumentaria", costo:2500},
+];
+
+const agregarProductos = ()=>{
+    let prenda = document.querySelector("#prenda").value;
+    let categoria = document.querySelector("#categoria");
+    let costo = parseFloat(document.querySelector("#costo").value);
+
+    let productoNuevo = new Producto(prenda, categoria, costo);
+    listaProductos.push(productoNuevo);
+    return productoNuevo;
+
+}
+listaProductos.forEach(el => {
+    document.createElement("div");
+nodo.innerHTML = `<h3>${el.prenda}</h3>
+                <h2>${el.categoria}<h2>
+                `
+})
